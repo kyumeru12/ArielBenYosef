75caddbbe1cbdc8655baca5670c53e726c58fb25
ib9001 <itaib9000@gmail.com>
2016-12-27 08:47:05 +0200

    Update DriveCommand.java

--- src/org/usfirst/frc/team5987/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team5987/robot/commands/DriveCommand.java
@@ -43,6 +43,6 @@ public class DriveCommand extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	cancel();
+    	end();
     }
 }
