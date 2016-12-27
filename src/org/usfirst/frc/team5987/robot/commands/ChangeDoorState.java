6bb01420e4140ecaa0d38ed6ef4794d5dcf07b1a
ib9001 <itaib9000@gmail.com>
2016-12-27 08:39:18 +0200

    Update ChangeDoorState.java

--- src/org/usfirst/frc/team5987/robot/commands/ChangeDoorState.java
+++ src/org/usfirst/frc/team5987/robot/commands/ChangeDoorState.java
@@ -59,6 +59,5 @@ public class ChangeDoorState extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	cancel();
     }
 }
